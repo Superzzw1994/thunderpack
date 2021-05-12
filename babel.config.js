8ac7799e5b2d9f3447274a911ca59ccbfcd747c3
邪恶天才 <shzx1994529@gmail.com>
2021-05-12 23:43:57 +0800

    Feature 增加sass，配置项目基本样式

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,22 @@
 const isProduction = (process.env.production === 'production')
 module.exports = {
-    presets: [
-        ["@babel/preset-env", {
-            useBuiltIns: 'usage',
-            corejs: 3
-        }],
-        ["@babel/preset-react"],
-        ["@babel/preset-typescript"]
-    ],
-    plugins: isProduction ? [] : [
-        ["react-refresh/babel"]
-    ]
+  presets: [
+    ["@babel/preset-env", {
+      useBuiltIns: 'usage',
+      corejs: 3
+    }],
+    ["@babel/preset-react"],
+    ["@babel/preset-typescript"]
+  ],
+  plugins: isProduction ? [["import", {
+    "libraryName": "antd",
+    "libraryDirectory": "es",
+    "style": true // `style: true` 会加载 less 文件
+  }]] : [
+    ["react-refresh/babel"], ["import", {
+      "libraryName": "antd",
+      "libraryDirectory": "es",
+      "style": true // `style: true` 会加载 less 文件
+    }]
+  ]
 }
